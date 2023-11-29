89a3d8508daa3fb2fd6a60fd8414d33c36899f51
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-11-29 16:55:22 +0500

    restocking action completed here

--- Redux/index.js
+++ Redux/index.js
@@ -1,5 +1,11 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 // ! Write the value same as its variable name it is a convention in old redux
 const CAKE_ORDERED = "CAKE_ORDERED";
+
+// ! Restocking the cakes or state you know the cake shop example
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 // * This is an action
 // ! It is dispatched when something has happened in our application
@@ -8,6 +14,14 @@ function orderCake() {
   return {
     type: CAKE_ORDERED,
     quantity: 1,
+  };
+}
+
+// ! Action to restock the cakes
+function restockCake(quantity = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    quantity,
   };
 }
 
@@ -27,7 +41,32 @@ const reducer = (state = initialState, action) => {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.quantity,
+      };
     default:
       return state;
   }
 };
+
+// ! creating store for the application and first time the reducer will return the initial state
+const store = createStore(reducer);
+
+// ? logging the initial state
+console.log("Initial state", store.getState());
+
+// ! subscribing to the store and storing the returned function in a variable which can then be called to unsubscribe from the store
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated new state", store.getState())
+);
+
+//* dispatching the actions
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(13));
+
+// ! unsubscribing from the store
+unsubscribe();
