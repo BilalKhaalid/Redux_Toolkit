9479b9a8753eed1d242fe59781c09d05cfe97e18
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-01 21:25:46 +0500

    view files for all slices and configured provider

--- React_Redux/src/App.jsx
+++ React_Redux/src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import CakeView from "./features/Cake/CakeView";
+import IceCreamView from "./features/IceCream/IceCreamView";
+import UserView from "./features/users/UserView";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CakeView />
+      <IceCreamView />
+      <UserView />
+    </div>
+  );
 };
 
 export default App;
