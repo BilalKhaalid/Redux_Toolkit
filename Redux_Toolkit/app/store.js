4b601ac123529284bd3d6e6a41e5854e41cf8573
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-01 17:10:44 +0500

    async with rtk completed here

--- Redux_Toolkit/app/store.js
+++ Redux_Toolkit/app/store.js
@@ -4,12 +4,14 @@ const cakeReducer = require("../features/Cake/cakeSlice");
 const IceCreamReducer = require("../features/IceCream/iceCreamSlice");
 const reduxLogger = require("redux-logger");
 const logger = reduxLogger.createLogger();
+const usersReducer = require("../features/users/usersSlice");
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     IceCream: IceCreamReducer,
+    users: usersReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 module.exports = store;
--- Redux_Toolkit/index.js
+++ Redux_Toolkit/index.js
@@ -2,15 +2,27 @@ const store = require("./app/store");
 const CakeActions = require("./features/Cake/cakeSlice").CakeActions;
 const IceCreamActions =
   require("./features/IceCream/iceCreamSlice").IceCreamActions;
+const fetchUsers = require("./features/users/usersSlice").fetchUsers;
 
 console.log("Initial State", store.getState());
 
 const unsubscribe = store.subscribe(() => {});
 
-store.dispatch(CakeActions.ordered(3));
-store.dispatch(CakeActions.restocked(6));
+// store.dispatch(fetchUsers());
 
-store.dispatch(IceCreamActions.ordered(3));
-store.dispatch(IceCreamActions.restocked(6));
+store
+  .dispatch(fetchUsers())
+  .then(() => {
+    console.log("Updated State", store.getState());
+  })
+  .catch((error) => {
+    console.error("Error fetching users:", error);
+  });
+
+// store.dispatch(CakeActions.ordered(3));
+// store.dispatch(CakeActions.restocked(6));
+
+// store.dispatch(IceCreamActions.ordered(3));
+// store.dispatch(IceCreamActions.restocked(6));
 
 unsubscribe();
